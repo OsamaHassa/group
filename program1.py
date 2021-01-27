0edebf8171aae14fb5985fc6b4c3f25fab052195
OsamaHassa <78023109+OsamaHassa@users.noreply.github.com>
2021-01-28 00:42:20 +0100

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 18:22:26 2021
+
+@author: oha003
+"""
+
+print("Option 1")
+print("Option 2")
+print("Option 3")
+c=input("Type the option")
+print(c)
+if c=="1":
+  print("You selected 1")
+  MyfunctionSine(100,1)
+if c=="2":
+  print("You selected 2")
+  PlotLetter("Group 1")
+if c=="3":
+  print("You selected 3")
+  print("Exit")
